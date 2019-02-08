d9453bd346e8a8e1c36856aab9ddbec2f841e2ee
Johnfelix Espinosa <johnfelix.espinosa@icloud.com>
2019-02-08 15:00:52 -0500

    Tracking SongAdapter, index.html, index.js

---
+++ src/adapters/SongsAdapter.js
@@ -0,0 +1,14 @@
+class SongsAdapter {
+  constructor() {
+    this.baseUrl = 
+    'http://localhost:3000/api/v1/songs'
+  };
+
+  getSongs() {
+    return fetch(this.baseUrl).then(res => res.json()
+    )
+  };
+}
+
+// adapter = new SongsAdapter()
+// const songs = adapter.getSongs()
---
+++ src/index.js
@@ -0,0 +1,6 @@
+const app = new App()
+
+//App flow
+//index js creates new instance of App
+//App once created, creates a new instance of songs
+//songs creates an array of songs using the songs adapter which fetches the api data with getSongs()
